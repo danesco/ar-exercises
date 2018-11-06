a2b0d190fca93b5dc9f44950d28e9b91a44a9536
danesco <danesco30@gmail.com>
2018-11-06 22:28:31 +0000

    finish first two exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,17 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+    # t.column :name, :string
+    # t.column :annual_revenue, :integer
+    # t.column :mens_apparel, :boolean
+    # t.column :womens_apparel, :boolean
+    # t.timestamps null: false
+
+burnaby = Store.create name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true
+
+richmond = Store.create name: "Richmond", annual_revenue: 120000, mens_apparel: false, womens_apparel: true
+
+gastown = Store.create name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false
+
+puts Store.count
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+p @store1
+
+@store2 = Store.find(2)
+
+p @store2
+
+@store1.name = "White Rock"
+@store1.save
+p @store1
