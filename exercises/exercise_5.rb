ebc1a1e6ea0e6b0e4075ab2a7a907b51734f2a03
Daniel Escobedo <danesco30@gmail.com>
2018-11-07 01:10:41 +0000

    finish execise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue).to_i
+over_one_mil = Store.where("annual_revenue > 1000000").size
+
+pp total_revenue
+pp average_revenue
+pp over_one_mil
